4140813fe6c56540407f9e8b5a746faca0f2d400
Alex Kiura <kiuraalex@gmail.com>
2019-02-25 15:35:46 +0300

    Clean up views

--- api_v1/views.py
+++ api_v1/views.py
@@ -3,11 +3,10 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Customer
-from .serializers import (CustomerSerializer, CustomerVerifySerializer,
-                          BankAccountSerializer, TransactionSerializer)
+from .serializers import CustomerSerializer, CustomerVerifySerializer
 from .utils import send_mail
 
-
+ 
 class CustomerCreateViewSet(viewsets.ModelViewSet):
     """
     API View that receives a POST with the following fields:
@@ -67,74 +66,5 @@ class CustomerCreateViewSet(viewsets.ModelViewSet):
                             status=status.HTTP_400_BAD_REQUEST)
 
 
-class CustomerVerifyViewSet(CustomerCreateViewSet):
-    """
-    API View that receives a POST with the following fields:
-        - email
-        - one time password
-    Verifies a user and returns a success messsage..
-    """
-    serializer_class = CustomerVerifySerializer
-
-    def verify(self, request):
-        email = request.data.get('email')
-        one_time_password = request.data.get('old_password')
-        new_password = request.data.get('new_password')
-        serializer = self.get_serializer(data=request.data)
-        if not serializer.is_valid():
-            return Response({'error':
-                             serializer.errors},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        try:
-            unverified_user = Customer.objects.get(email=email)
-            unverified_user.verify(one_time_password, new_password)
-            return Response({
-                    'email': email,
-                    'verified': unverified_user.is_verified
-                },
-                status=status.HTTP_200_OK)
-
-        except Exception as error:
-            return Response({
-                'error': [error.args]},
-                status=status.HTTP_400_BAD_REQUEST
-            )
 
 
-
-    queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer
-    permission_classes = (IsAuthenticated,)
-
-    def list(self, request):
-        transactions = Transaction.objects.filter(
-            account__owner=request.user).all()
-        serializer = self.get_serializer(transactions, many=True)
-        return Response(serializer.data)
-
-    def do_transact(self, request):
-        amount = request.data.get('amount')
-        transaction_type = request.data.get('transaction_type')
-        account_id = request.data.get('account')
-        description = request.data.get('description')
-        serializer = self.get_serializer(data=request.data)
-        if not serializer.is_valid():
-            return Response({'error':
-                             serializer.errors},
-                            status=status.HTTP_400_BAD_REQUEST)
-        try:
-            account = BankAccount.objects.get(id=account_id)
-            transaction = Transaction.objects.create(
-                amount=amount,
-                transaction_type=transaction_type,
-                account=account,
-                description=description
-            )
-            serializer = TransactionSerializer(transaction)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        except Exception as error:
-            return Response({
-                'error': [error.args]},
-                status=status.HTTP_400_BAD_REQUEST
-            )

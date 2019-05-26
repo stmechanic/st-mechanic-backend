e8784df955d0e218c4bf1a27e24ef0b9d4daa164
Alex Kiura <kiuraalex@gmail.com>
2019-05-26 19:20:11 +0300

    Add Garage serializer

--- garage/serializers.py
+++ garage/serializers.py
@@ -1,14 +1,10 @@
+"""API Serializers."""
 from rest_framework import serializers
+from .models import Garage
 
 
 class GarageSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=255)
-    specialties = serializers.ArrayField(serializers.CharField)
-    username = None
-    email = serializers.EmailField(unique=True, max_length=254)
-    registration_number = serializers.CharField(max_length=20, unique=True)
-    physical_address = serializers.CharField(
-        max_length=255, blank=True, null=True)
-    verified = serializers.BooleanField(default=False)
-    opening_time = serializers.TimeField()
-    closing_time = serializers.TimeField()
+    """Serialize Garage instances."""
+    class Meta:
+        model = Garage
+        # fields = ('') 

4579d3cd86cfa4836c0a4070b8ecfc6601718e9a
Alex Kiura <kiuraalex@gmail.com>
2019-05-26 14:33:57 +0300

    Fix lint issues

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ DEBUG = config('DEBUG', cast=bool)
 
 SENDGRID_API_KEY = config('SENDGRID_API_KEY')
 
-ALLOWED_HOSTS=['.localhost', '.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.localhost', '.herokuapp.com', '127.0.0.1']
 
 # Application definition
 PREREQ_APPS = [
